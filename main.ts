b52088029b8efad35db763df7d8c16ef76009132
MrDGuy <84729967+MrDGuy@users.noreply.github.com>
2023-09-26 10:06:37 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-let is_weekday = game.ask("Is it a weekday?", "OK = True, Cancel = False")
-let is_holiday = game.ask("Is it a holiday?", "OK = True, Cancel = False")
-if (is_weekday && !is_holiday) {
-    adventure.addToTextlog("There is school today.")
-} else {
-    adventure.addToTextlog("There is no school today.")
-}
 
